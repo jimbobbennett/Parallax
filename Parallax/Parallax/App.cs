29e99e7ea2053efbbaa3160df84519a056e158c8
Jim Bennett <jimbobbennett@mac.com>
2015-05-16 14:58:24 +0100

    first cut

---
+++ Parallax/Parallax/App.cs
@@ -0,0 +1,43 @@
+﻿using Xamarin.Forms;
+
+namespace Parallax
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var label = new Label
+            {
+                Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et " +
+                       "dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip " +
+                       "ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore " +
+                       "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia " +
+                       "deserunt mollit anim id est laborum."
+            };
+
+            label.FontSize = Device.GetNamedSize(NamedSize.Large, label);
+
+            MainPage = new ParallalPage
+            {
+                ScrollContent = label,
+                ImageSource = "r2d2.jpg"
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Parallax/Parallax/ParallalPage.cs
@@ -0,0 +1,85 @@
+﻿using Xamarin.Forms;
+
+namespace Parallax
+{
+    [ContentProperty("ScrollContent")]
+    public class ParallalPage : ContentPage
+    {
+        private readonly Grid _grid;
+        private readonly Image _image;
+        private readonly ScrollView _scrollView;
+        private readonly ContentView _contentView;
+
+        public ParallalPage()
+        {
+            _grid = new Grid
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.Fill
+            };
+
+            _image = new Image
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.Start,
+                Aspect = Aspect.AspectFit
+            };
+
+            _scrollView = new ScrollView
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                IsClippedToBounds = true
+            };
+
+            _contentView = new ContentView
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+
+            _scrollView.Content = _contentView;
+
+            _image.SizeChanged += (s, e) =>
+            {
+                _contentView.Padding = new Thickness(0, _image.Height, 0, 0);
+            };
+
+            _grid.Children.Add(_image);
+            _grid.Children.Add(_scrollView);
+
+            Content = _grid;
+        }
+
+        public static readonly BindableProperty ImageSourceProperty =  BindableProperty.Create<ParallalPage, ImageSource>(p => p.ImageSource, null, 
+            propertyChanged:OnImageSourceChanged);
+
+        private static void OnImageSourceChanged(BindableObject bindable, ImageSource oldvalue, ImageSource newvalue)
+        {
+            ((ParallalPage) bindable)._image.Source = newvalue;
+        }
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource) GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        private View _scrollContent;
+
+        public View ScrollContent
+        {
+            get { return _scrollContent; }
+            set
+            {
+                if (_scrollContent == value) return;
+
+                _scrollContent = value;
+                _contentView.Content = _scrollContent;
+                _scrollContent.VerticalOptions = LayoutOptions.FillAndExpand;
+
+                OnPropertyChanged();
+            }
+        }
+    }
+}
